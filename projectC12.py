a3392345013a0fc32293d66b0aedba9b2218d650
Erik Bjerned <erbj0497@arrhenius.it.uu.se>
2022-12-21 12:31:10 +0100

    Fine mesh C12

--- projectC12.py
+++ projectC12.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from dolfin import * 
 from fenics import *
 import numpy as np
-mesh = Mesh("meshes/circle.xml.gz")
+mesh = Mesh("meshes/circle_fine.xml.gz")
 P1 = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
 element = MixedElement([P1, P1, P1])
 
