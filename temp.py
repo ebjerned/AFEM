64e4d20e4d30f737a68339c6887eb33f9407dbce
Erik Bjerned <erbj0497@arrhenius.it.uu.se>
2023-01-06 11:50:19 +0100

    Wrong data C12

--- temp.py
+++ temp.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-up = np.loadtxt("results_C12/C12mutualist.txt")
-vp = np.loadtxt("results_C12/C12prey.txt")
-wp = np.loadtxt("results_C12/C12pred.txt")
-t = np.loadtxt("results_C12/C12time.txt")
+up = np.loadtxt("results_sweden/Swedenmutualist.txt")
+vp = np.loadtxt("results_sweden/Swedenprey.txt")
+wp = np.loadtxt("results_sweden/Swedenpred.txt")
+t = np.loadtxt("results_sweden/Swedentime.txt")
 
 plt.plot(t,up)
 plt.plot(t,vp)
@@ -13,7 +13,7 @@ plt.legend(["Mutualist", "Prey", "Predator"])
 plt.title("Population over time")
 plt.xlabel("Time")
 plt.ylabel("Population")
-plt.savefig("results_C12/p_tot.png")
+plt.savefig("results_sweden/p_tot.png")
 
 plt.clf()
 
@@ -21,9 +21,9 @@ plt.plot(t,up)
 plt.plot(t,vp)
 plt.plot(t,wp)
 ax = plt.gca()
-ax.set_xlim([0, 150])
+ax.set_xlim([0, 200])
 plt.legend(["Mutualist", "Prey", "Predator"])
 plt.title("Population over time")
 plt.xlabel("Time")
 plt.ylabel("Population")
-plt.savefig("results_C12/p_tot_zoomed.png")
+plt.savefig("results_sweden/p_tot_zoomed.png")
