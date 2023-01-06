d90f4a459302a6bb6e1850a418b1c59689887e07
Erik Bjerned <erbj0497@arrhenius.it.uu.se>
2023-01-06 11:40:48 +0100

    Plots

---
+++ temp.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+up = np.loadtxt("results_C12/C12mutualist.txt")
+vp = np.loadtxt("results_C12/C12prey.txt")
+wp = np.loadtxt("results_C12/C12pred.txt")
+t = np.loadtxt("results_C12/C12time.txt")
+
+plt.plot(t,up)
+plt.plot(t,vp)
+plt.plot(t,wp)
+plt.legend(["Mutualist", "Prey", "Predator"])
+plt.title("Population over time")
+plt.xlabel("Time")
+plt.ylabel("Population")
+plt.savefig("results_C12/p_tot2.png")
